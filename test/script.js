9b655b8608216c022db3deedf3208691c2f4d911
naotot <naototakahashi825@gmail.com>
2017-10-12 10:58:51 +0800

    Add

---
+++ test/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var dataURL = 'https//api.instagram.com/v1/users/self/media/recent';
+	var phtoData;
+
+	var get Data = function(url) {
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			data: {
+				access_token: '317491.3df75a0.80f8332dfc3b45a7abfe0ba4b3e84234',
+				count: 12
+			}
+
+		})
+		.done(function(data){
+			photoData = data;
+			console.dir(photoData);
+		})
+		.fail(function() {
+			$('#gallery').text(textStatus);
+		})
+	}
+
+	getData(dataURL);
+});
